b0d0f12ee9ef7b8dc51b5277f3d6ba706eb12175
gabri <gabri@10.232.4.234>
2023-02-22 11:03:57 -0700

    correccion de errores

---
+++ src/mx/itson/CFEproMax/entidades/Factura.java
@@ -0,0 +1,15 @@
+package mx.itson.CFEproMax.entidades;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author axelt
+ */
+public class Factura {
+    
+
+}
